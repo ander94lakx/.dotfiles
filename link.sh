80e6633e964f4a8435547736a3b0baeedb0875f8
Ander Granado <ander@protonmail.com>
2022-12-08 23:48:13 +0100

    Adds structure and more symlinks

--- link.sh
+++ link.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
 
+# Git
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
+# ZSH
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.p10k.zsh ~/.p10k.zsh
+
+# Tmux
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.tmux ~/
+
+# Vim
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/.vim ~/.vim
+ln -s ~/.dotfiles/.vim ~/
+
+# Editorconfig
+ln -s ~/.dotfiles/.editorconfig ~/.editorconfig
