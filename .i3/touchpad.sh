8b8a302f8ae34ecd9a8dc235ad07c7d01fc098f0
Ander <ander@protonmail.com>
2024-06-26 16:21:46 +0200

    Fixes touchpad device retrieving grep command

--- .i3/touchpad.sh
+++ .i3/touchpad.sh
@@ -4,7 +4,7 @@
 # Thanks Maxwell! (https://github.com/maxwell-bland/i3-natural-scrolling-and-tap)
 
 # Get id of touchpa
-id=`xinput list | grep -i "Touchpad" | cut -d'=' -f2 | cut -d'[' -f1`
+id=`xinput list | grep -i "Touchpad" | head -1 | cut -d'=' -f2 | cut -d'[' -f1`
 
 # Get the id of the field corresponding to natural scrolling
 natural_scrolling_id=`xinput list-props $id | \
@@ -15,9 +15,6 @@ natural_scrolling_id=`xinput list-props $id | \
 tap_to_click_id=`xinput list-props $id | \
                       grep -i "Tapping Enabled (" \
                       | cut -d'(' -f2 | cut -d')' -f1`
-echo $natural_scrolling_id
-
-echo $tap_to_click_id
 
 # Set noth properties y to true
 xinput set-prop $id $tap_to_click_id 1
